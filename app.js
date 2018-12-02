e70c272996f1fc846af3b4be90fe36734af605a1
Douglas Naphas <douglasnaphas@gmail.com>
2018-12-02 08:22:40 -0500

    Add public & protected endpoints for testing
    madliberation-50

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    Output: 'Hello World!'
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    Output: 'Hello World!'
   });
 });
 
+app.get('/public-endpoint', function(req, res) {
+  res.send({
+    Output: 'this endpoint is public'
+  });
+});
+
+app.get('/protected-endpoint', function(req, res) {
+  res.send({
+    Output: 'this endpoint is protected'
+  });
+});
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app
+module.exports = app;
