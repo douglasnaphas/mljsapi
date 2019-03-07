0586563c900fea090f30faf345a83edc3ad00966
Douglas Naphas <douglasnaphas@gmail.com>
2019-03-07 03:10:41 +0000

    Rename Schema for case-insensitive filesystems
    Having both schema.js and Schema.js confused Mac

--- DbSchema.js
+++ DbSchema.js
@@ -1,8 +1,8 @@
 const schema = require('./schema');
 const getHash = require('./lib/getHash');
-class Schema {
+class DbSchema {
   static sortKeyFromGameName(gameName) {
     return schema.PARTICIPANT_PREFIX + schema.SEPARATOR + getHash(gameName);
   }
 }
-module.exports = Schema;
+module.exports = DbSchema;
